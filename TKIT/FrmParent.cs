37eb28b9968ce6150ead32b863c4b9c5adea48b8
at3158 <tk290@tkfood.com.tw>
2021-10-06 09:14:45 +0800

    TKSYSPRUSED

--- TKIT/FrmParent.cs
+++ TKIT/FrmParent.cs
@@ -11,6 +11,8 @@ using System.Configuration;
 using System.Reflection;
 using System.Diagnostics;
 using TKITDLL;
+using System.Net;
+using System.Net.Sockets;
 
 namespace TKIT
 {
@@ -98,7 +100,9 @@ namespace TKIT
 
 
             //ADD USED LOG
-            TKSYSPRUSED("TKIT", dtransaction.Rows[0]["FRM_CODE"].ToString(), sender.ToString(), UserName);
+            List<string> IPAddress = GetHostIPAddress();
+            //MessageBox.Show(IPAddress[0].ToString());            
+            TKSYSPRUSED(MethodBase.GetCurrentMethod().DeclaringType.Namespace, dtransaction.Rows[0]["FRM_CODE"].ToString(), sender.ToString(), UserName, IPAddress[0].ToString());
 
             Assembly frmAssembly = Assembly.LoadFile(Application.ExecutablePath);
             foreach (Type type in frmAssembly.GetTypes())
@@ -142,7 +146,7 @@ namespace TKIT
 
         }
 
-        public void TKSYSPRUSED(string SYSTEMNAME,string PROGRAMCODE, string PROGRAMNAME, string USEDID)
+        public void TKSYSPRUSED(string SYSTEMNAME,string PROGRAMCODE, string PROGRAMNAME, string USEDID,string USEDIP)
         {
             SqlConnection sqlConn = new SqlConnection();
             SqlTransaction tran;
@@ -173,9 +177,9 @@ namespace TKIT
 
             sbSql.AppendFormat(@" 
                                 INSERT INTO [TKIT].[dbo].[TKSYSPRUSED]
-                                ([SYSTEMNAME],[PROGRAMCODE],[PROGRAMNAME],[USEDDATES],[USEDID])
+                                ([SYSTEMNAME],[PROGRAMCODE],[PROGRAMNAME],[USEDDATES],[USEDID],[USEDIP])
                                 VALUES
-                                (@SYSTEMNAME,@PROGRAMCODE,@PROGRAMNAME,@USEDDATES,@USEDID)
+                                (@SYSTEMNAME,@PROGRAMCODE,@PROGRAMNAME,@USEDDATES,@USEDID,@USEDIP)
                                 ");
 
 
@@ -187,6 +191,7 @@ namespace TKIT
                 command.Parameters.AddWithValue("@PROGRAMNAME", PROGRAMNAME);
                 command.Parameters.AddWithValue("@USEDDATES", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
                 command.Parameters.AddWithValue("@USEDID", USEDID);
+                command.Parameters.AddWithValue("@USEDIP", USEDIP);
 
                 try
                 {
@@ -206,6 +211,26 @@ namespace TKIT
             }
                       
             
+        }
+
+        // <summary>
+        /// 取得本機 IP Address
+        /// </summary>
+        /// <returns></returns>
+        private List<string> GetHostIPAddress()
+        {
+            List<string> lstIPAddress = new List<string>();
+            IPHostEntry IpEntry = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ipa in IpEntry.AddressList)
+            {
+                if (ipa.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    lstIPAddress.Add(ipa.ToString());
+                    //MessageBox.Show(ipa.ToString());
+                }
+                   
+            }
+            return lstIPAddress; // result: 192.168.1.17 ......
         }
 
     }
